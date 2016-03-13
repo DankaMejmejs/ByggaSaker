0bd268a5ef80cc089458dd9b8daa8b83d625f644
Jeflux <jespermathiason@gmail.com>
2016-03-13 14:13:04 +0100

    fysik

--- TowerBollocks/Source/TowerBollocks/BrickActor.cpp
+++ TowerBollocks/Source/TowerBollocks/BrickActor.cpp
@@ -21,6 +21,7 @@ ABrickActor::ABrickActor()
 	FTransform trans = FTransform();
 	trans.SetScale3D(FVector(1.0f, 0.5f, 0.5f));
 	block->SetRelativeTransform(trans);
+	block->BodyInstance.bSimulatePhysics = true;
 	//block->BodyInstance.bLockXTranslation = true;
 	//block->BodyInstance.bLockYTranslation = true;
 	//block->BodyInstance.bLockZRotation = true;
@@ -47,13 +48,15 @@ void ABrickActor::Tick( float DeltaTime )
 		transform = GetTransform();
 
 	if (isHeld) {
-		block->BodyInstance.bSimulatePhysics = false;
+		if (Role == ROLE_Authority)
+			block->BodyInstance.bSimulatePhysics = false;
 		block->BodyInstance.SetEnableGravity(false);
 		block->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
 	}
 	else {
-		block->BodyInstance.bSimulatePhysics = true;
+		if (Role == ROLE_Authority)
+			block->BodyInstance.bSimulatePhysics = true;
 		block->BodyInstance.SetEnableGravity(true);
 		block->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 	}
